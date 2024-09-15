16e005b00e118c46d7025881891bde38462f1c3c
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2024-09-15 20:47:54 +0900

    Title: 배열 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240915/배열 채우기/filling-array.py
+++ 240915/배열 채우기/filling-array.py
@@ -6,7 +6,11 @@ for i in range(len(arr)):
     if arr[i] == 0:
         idx = i-1
 
-
-for _ in range(idx+1):
-    print(arr[idx], end=" ")
-    idx -= 1
+if idx != len(arr):
+    for _ in range(idx+1):
+        print(arr[idx], end=" ")
+        idx -= 1
+else:
+    for _ in range(idx):
+        print(arr[idx], end=" ")
+        idx -= 1
