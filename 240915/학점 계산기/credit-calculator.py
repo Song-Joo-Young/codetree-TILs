5fa119bf0d78ba74a22846ed5d28cbc215fe7dfa
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2024-09-15 20:26:38 +0900

    Title: 학점 계산기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/학점 계산기/credit-calculator.py
+++ 240915/학점 계산기/credit-calculator.py
@@ -3,9 +3,9 @@ n = int(input())
 grade = list(map(float, input().split()))
 average = round(sum(grade)/len(grade),1)
 print(average)
-if average > 4.0:
+if average >= 4.0:
     print("Perfect")
-elif average > 3.0:
+elif average >= 3.0:
     print("Good")
 else:
     print("Poor")
