c6b53d226f4cd9f6cf6003e4ab4190ba55889031
Song <64202860+Song-Joo-Young@users.noreply.github.com>
2024-09-15 21:03:18 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240915/시험 통과 여부 확인하기 2/verify-test-passed-2.py
+++ 240915/시험 통과 여부 확인하기 2/verify-test-passed-2.py
@@ -5,10 +5,10 @@ cnt = 0
 for _ in range(n):
     grade = list(map(int, input().split()))
     avg = sum(grade)/4
-    if avg >= 60
+    if avg >= 60:
         print("pass")
         cnt += 1
     else:
         print("fail")
-        
+
 print(cnt)
